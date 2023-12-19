c59c2e19258933602425f6bb27e6d61e181a799a
ndnhattan <125990067+ndnhattan@users.noreply.github.com>
2023-12-19 19:36:12 +0700

    Config router and project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import PublicLayout from "./layouts/PublicLayout";
+import { Home, About } from "./pages";
 
 export default function App() {
   return (
@@ -8,21 +9,8 @@ export default function App() {
         <Route path="/" element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
         </Route>
       </Routes>
     </div>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -11,9 +11,6 @@ const Header = () => {
           <li>
             <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
         </ul>
       </nav>
     </div>
